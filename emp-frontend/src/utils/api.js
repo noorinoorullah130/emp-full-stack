e2fe18b7d83e58816fc98405d9e0e9b447defa19
Noorullah Noori <164587782+noorinoorullah130@users.noreply.github.com>
2026-01-17 13:38:49 +0430

    Change API base URL to production endpoint

--- emp-frontend/src/utils/api.js
+++ emp-frontend/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getTokenAndRole } from "./auth";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://emp-backend-mdoo.onrender.com",
 });
 
 api.interceptors.request.use(
